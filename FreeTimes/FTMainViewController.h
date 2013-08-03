bdd53bf96f010bf74b61aaedc82e73b6bdfd44c4
Zak Auerbach <zak@zaksoup.com>
2013-08-03 16:02:11 -0400

    Add MainViewController

---
+++ FreeTimes/FTMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTMainViewController.h
+//  FreeTimes
+//
+//  Created by Zak Auerbach on 8/3/13.
+//  Copyright (c) 2013 Zak Auerbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTMainViewController : UIViewController
+
+@end
